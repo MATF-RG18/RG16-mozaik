d19d63c640fa7b2455d9799084f57e9b90938ad9
aleksandar-stefanovic <theonewithideas@gmail.com>
2018-11-05 14:04:20 +0100

    Added buffer and shader initialization.

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,9 @@
 #pragma clang diagnostic push
 #pragma ide diagnostic ignored "OCUnusedMacroInspection"
 #define GLEW_STATIC
+
+void init_shaders();
+
 #pragma clang diagnostic pop
 #include <GL/glew.h>
 
@@ -28,8 +31,19 @@ int main() {
     glewExperimental = GL_TRUE;
     glewInit();
 
+    // Initialize buffers
+    GLuint vertex_buffer;
+    glGenBuffers(1, &vertex_buffer);
+    glBindBuffer(GL_ARRAY_BUFFER, vertex_buffer);
+    // TODO: buffer vertex array to display vertices.
+
+    // Initialize shaders
+    init_shaders();
+
     // Main loop
     while (!glfwWindowShouldClose(window)) {
+        // TODO: draw something.
+
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
@@ -37,4 +51,67 @@ int main() {
     glfwTerminate();
     // TODO: return glError for debugging purposes
     return 0;
+}
+
+void init_shaders() {
+    // TODO: move to a file in the future
+    const char* vertex_shader_source = R"glsl(
+        #version 150 core
+
+        in vec3 position;
+
+        void main() {
+            /* Fourth coordinate is related to clipping and should default to 1.0 */
+            gl_Position = vec4(position, 1.0);
+        }
+    )glsl";
+
+    GLuint vertex_shader = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertex_shader, 1, &vertex_shader_source, nullptr);
+    glCompileShader(vertex_shader);
+
+    // Check whether the shader compiled successfully
+    GLint status;
+    glGetShaderiv(vertex_shader, GL_COMPILE_STATUS, &status);
+    if (status != GL_TRUE) {
+        char buffer[4096];
+        glGetShaderInfoLog(vertex_shader, 4096, nullptr, buffer);
+        printf("Vertex buffer compilation failed:\n%s", buffer);
+    }
+
+    // TODO: move to a file in the future
+    const char* fragment_shader_source = R"glsl(
+        #version 150 core
+
+        out vec4 out_color;
+
+        void main() {
+            /* Just white for now */
+            out_color = vec4(1.0, 1.0, 1.0, 1.0);
+        }
+    )glsl";
+
+    GLuint fragment_shader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragment_shader, 1, &fragment_shader_source, nullptr);
+    glCompileShader(fragment_shader);
+
+    // Check whether the shader compiled successfully (using status from before)
+    glGetShaderiv(fragment_shader, GL_COMPILE_STATUS, &status);
+    if (status != GL_TRUE) {
+        char buffer[4096];
+        glGetShaderInfoLog(fragment_shader, 4096, nullptr, buffer);
+        printf("Fragment buffer compilation failed:\n%s", buffer);
+    }
+
+    GLuint shader_program = glCreateProgram();
+    glAttachShader(shader_program, vertex_shader);
+    glAttachShader(shader_program, fragment_shader);
+
+    // Fragment shader can output to multiple buffers, so specifying buffer locations is
+    // necessary. However, 0 is used by default, so the next statement is kept for
+    // clarity only.
+    glBindFragDataLocation(shader_program, 0, "out_color");
+
+    glLinkProgram(shader_program);
+    glUseProgram(shader_program);
 }
