605f67acd9b54dd7fbea9abc3035b1b9fad2cd48
aleksandar-stefanovic <theonewithideas@gmail.com>
2018-11-05 12:49:41 +0100

    Dodat CMake build system

--- README.md
+++ README.md
@@ -2,3 +2,12 @@
 # Mozaik
 
 U ovom projektu, korisnik crta izlomljene linije po ravni, i kada od tih izlomljenih linija napravi poligon, može da taj poligon oboji. Boja se bira sa (aproksimacije) sfere, gde je azimut nijansa, pozitivni deo visine je svetlina, a negativni deo visine je jarkost boje.
+
+## Build
+
+```
+mkdir build
+cd build
+cmake ..
+make
+```
--- src/main.cpp
+++ src/main.cpp
@@ -1,29 +1,6 @@
-#include <GL/glut.h>
+#include <iostream>
 
-void on_display();
-
-int main(int argc, char** argv) {
-    glutInit(&argc, argv);
-    glutInitDisplayMode(GLUT_RGB | GLUT_DOUBLE);
-
-    glutCreateWindow(argv[0]);
-
-    glutDisplayFunc(on_display);
-
-    glutMainLoop();
-
-}
-
-void on_display() {
-    // Briše se sadržaj od ranije
-    glClear(GL_COLOR_BUFFER_BIT);
-
-    glColor3f(1.0f, 1.0f, 1.0f);
-    glBegin(GL_POLYGON);
-        glVertex2f(-0.5f, -0.5f);
-        glVertex2f(0.5f, -0.5f);
-        glVertex2f(0.5f, 0.5f);
-        glVertex2f(-0.5f, 0.5f);
-    glEnd();
-    glutSwapBuffers();
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
 }
