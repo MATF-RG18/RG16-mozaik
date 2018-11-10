b06fae5c3b1a3e7c81492d2a9af595db69d5bd94
aleksandar-stefanovic <theonewithideas@gmail.com>
2018-11-10 21:37:05 +0100

    Copied the HSV to RGB function from the Web.
    Taken from https://github.com/hughsk/glsl-hsv2rgb/blob/master/index.glsl

--- src/main.cpp
+++ src/main.cpp
@@ -167,6 +167,12 @@ GLuint init_shaders() {
             float color_value = (vector_proj.x + 1) / 2;
             Color = vec3(color_value);
         }
+        /* Taken from https://github.com/hughsk/glsl-hsv2rgb/blob/master/index.glsl */
+        vec3 hsv2rgb(vec3 c) {
+            vec4 K = vec4(1.0, 2.0 / 3.0, 1.0 / 3.0, 3.0);
+            vec3 p = abs(fract(c.xxx + K.xyz) * 6.0 - K.www);
+            return c.z * mix(K.xxx, clamp(p - K.xxx, 0.0, 1.0), c.y);
+        }
     )glsl";
 
     GLuint vertex_shader = glCreateShader(GL_VERTEX_SHADER);
